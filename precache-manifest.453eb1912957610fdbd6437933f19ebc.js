eb6d106212b99b78fc0f8e4e52df2dc74ceba512
Ric Mershon <rmersh@ricmershon.com>
2020-06-28 16:00:22 -0400

    Updates

---
+++ precache-manifest.453eb1912957610fdbd6437933f19ebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a775d3463468fc73a36ce2d9fab4cdeb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5ceb63627528889470f8",
+    "url": "/monsters-rolodex/static/css/main.2ab847a4.chunk.css"
+  },
+  {
+    "revision": "b7025d703d6faac98ebe",
+    "url": "/monsters-rolodex/static/js/2.a0f84a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a0f84a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ceb63627528889470f8",
+    "url": "/monsters-rolodex/static/js/main.1207030f.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
